fb4b9661548f2f01453ad01364db905de6d5f3eb
Andrew.Kalashnikov <it-zodiac@yandex.ru>
2023-05-05 17:43:04 +0600

    исправил ошибки, добавил комментарии

--- Homeworks/app/Task_1-2-D+.py
+++ Homeworks/app/Task_1-2-D+.py
@@ -19,19 +19,21 @@ my_favorite_songs_dict = {
     'In This World': 4.02,
 }
 
-# Выводим выбранные песни и их длительность на консоль
-for song, duration in my_favorite_songs_dict.items():
-    minutes = int(duration)
-    seconds = int((duration - minutes) * 60)
-    print(f'{song}: {minutes:02d}:{seconds:02d}')
+
+for song, duration in my_favorite_songs_dict.items(): # для каждой песни берём длительность
+    minutes = int(duration)                     # целая часть - это минуты
+    seconds = int((duration - minutes) * 60)    # дробная часть - это секунды, но это не 100-е доли, 
+                                                # а 60-е, поэтому умножаем на 60 
+    # Выводим песни и их длительность на консоль
+    print(f'{song} - {minutes:02d}:{seconds:02d}')
 
 # Результат:
-# Waste a Moment: 03:03
-# New Salvation: 04:02
-# Staying' Alive: 03:40
-# Out of Touch: 03:03
-# A Sorta Fairytale: 05:28
-# Easy: 04:15
-# Beautiful Day: 04:04
-# Nowhere to Run: 02:58
-# In This World: 04:02
+# Waste a Moment - 03:01
+# New Salvation - 04:01
+# Staying' Alive - 03:23
+# Out of Touch - 03:01
+# A Sorta Fairytale - 05:16
+# Easy - 04:09
+# Beautiful Day - 04:02
+# Nowhere to Run - 02:34
+# In This World - 04:01
