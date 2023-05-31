b3355b487efc6bb424cb54cd96f54b3eee6fe636
Andrew.Kalashnikov <it-zodiac@yandex.ru>
2023-05-31 16:17:45 +0600

    Исправил решение задачи 8

--- lesson12_29_05_23/Task_8.py
+++ lesson12_29_05_23/Task_8.py
@@ -1,12 +1,12 @@
 # Задача 8
 import sqlite3
 import pandas as pd
-con = sqlite3.connect("Students.db")
+con1 = sqlite3.connect("Students.db")
 con2 = sqlite3.connect("Schools.db")
 
-df1 = pd.read_sql('SELECT * FROM Students', con)
+df1 = pd.read_sql('SELECT * FROM Students', con1)
 df1.to_sql("Students", con2, if_exists="append", index = False)
 
-df2 = pd.read_sql('SELECT Students.Student_name, Students.School_id, School.School_name FROM Students JOIN School ON Students.School_id = School.School_id', con)
+df2 = pd.read_sql('SELECT Students.Student_name, Students.School_id, School.School_name FROM Students JOIN School ON Students.School_id = School.School_id', con2)
 print(df2)
 #df2.to_excel('Task8.xlsx', index = False)
